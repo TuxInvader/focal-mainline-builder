0457209467c471c67d870716811b8a678474afe8
TuxInvader <github@badpenguin.co.uk>
2023-03-19 13:29:31 +0000

    cleanup

--- build.sh
+++ build.sh
@@ -23,6 +23,7 @@ metaonly=no
 maintainer="Zaphod Beeblebrox <zaphod@betelgeuse-seven.western-spiral-arm.change.me.to.match.signing.key>"
 buildargs="-aamd64 -d"
 
+
 do_metapackage() {
   KVER=$1
   METAVER=$2
@@ -36,13 +37,7 @@ do_metapackage() {
   BINS="${KVER}-${ABINUM}-${FLAVOUR}"
   DEPS="linux-headers-${BINS}, linux-image-unsigned-${BINS}, linux-modules-${BINS}"
 
-  # fix jammy equivs-build
-  # Blame: https://salsa.debian.org/perl-team/modules/packages/equivs/-/commit/a99d769bbf7c0ef30502c61fdcbb880be0e00e8e
-  #if [ "$SERIES" == "jammy" ]
-  #then
-  # sed -i -re 's/-uc -us//g' /usr/bin/equivs-build
-  #fi
-
+  echo ">>> Metapackage for $FLAVOUR: MetaVersion: $METAVER, MetaTime: $METATIME"
   [ -d "../meta" ] || mkdir ../meta
   cd ../meta
   cat > metapackage.control <<-EOF
@@ -96,7 +91,7 @@ do_metapackage() {
   equivs-build metapackage.control
   if [ "$BTYPE" == "source" ]
   then
-    echo "Building source"
+    echo ">>> Building source"
     equivs-build --source metapackage.control
   fi
 
@@ -118,6 +113,17 @@ __die() {
   printf 1>&2 '%s\n' "ERROR: $*"; exit $rc
 }
 
+echo -e "********\n\nBuild starting\n\n********"
+
+echo ">>> Setting haverust flag (default no on focal)"
+# Default haverust to no on focal
+if [ "$series" == "focal" ]
+then
+  haverust=${haverust:-"no"}
+else
+  haverust=${haverust:-"yes"}
+fi
+
 args=( "$@" );
 for (( i=0; $i < $# ; i++ ))
 do
@@ -127,7 +133,7 @@ do
     key=${arg#--}
     val=${key#*=}; key=${key%%=*}
     case "$key" in
-      update|btype|shell|custom|sign|flavour|exclude|rename|patch|series|checkbugs|buildmeta|maintainer|debug|kver|metaver|metaonly|metatime)
+      update|btype|shell|custom|sign|flavour|exclude|rename|patch|series|checkbugs|buildmeta|maintainer|debug|kver|metaver|metaonly|metatime|haverust)
         printf -v "$key" '%s' "$val" ;;
       *) __die 1 "Unknown flag $arg"
     esac
@@ -135,6 +141,7 @@ do
   fi
 done
 
+echo -e ">>> Args.... sign is $sign"
 if [ "$sign" == "no" ]
 then
   buildargs="$buildargs -uc -ui -us"
@@ -154,6 +161,7 @@ echo -e "********\n\nCleaning git source tree\n\n********"
 git clean -fdx || __die 1 'git failed'
 git reset --hard HEAD
 
+echo -e ">>> Args.... update is $update"
 if [ "$update" == "yes" ]
 then
   echo -e "********\n\nUpdating git source tree\n\n********"
@@ -165,6 +173,7 @@ echo -e "********\n\nSwitching to cod/mainline/${kver} branch\n\n********"
 git checkout "cod/mainline/${kver}" || __die 1 "Tag for 'cod/mainline/${kver} not found"
 
 # Apply patch if requested
+echo -e ">>> Args.... patch is $patch"
 if [ "$patch" != "no" ]
 then
   echo -e "********\n\nDownloading and patching cod/mainline/${kver} to $patch\n\n********"
@@ -187,6 +196,7 @@ fi
 echo -e "********\n\nRenaming source package and updating control files\n\n********"
 debversion=$(date +%Y%m%d%H%M)
 abinum=$(echo ${kver:1} | awk -F. '{printf "%02d%02d%02d", $1,$2,$3 }')
+echo -e ">>> Args.... rename is $rename"
 if [ "$rename" == "yes" ]
 then
   sed -i -re "s/(^linux) \(([0-9]+\.[0-9]+\.[0-9]+)-([^\.]*)\.[0-9]+\) ([^;]*)(.*)/linux-${kver:1} (${kver:1}-${abinum}.${debversion}) ${series}\5/" debian.master/changelog
@@ -202,16 +212,20 @@ fi
 sed -i -re 's/dwarves \[/dwarves (>=1.21) \[/g' debian.master/control.stub.in
 
 # undo GCC-11 update in focal
+echo -e ">>> Args.... series is $series"
 if [ "$series" == "focal" ]
 then
+  echo -e ">>> Downgrade GCC to version 9 on focal"
   sed -i -re 's/export gcc\?=.*/export gcc?=gcc-9/' debian/rules.d/0-common-vars.mk
 fi
 
-# Revert rust dependencies on focal for 6.1+
-if [ "$series" == "focal" ]
+# Remove rust dependencies if $haverust is "no". Defaults to "no" on focal and "yes" on others.
+echo -e ">>> Args.... haverust is $haverust"
+if [ "$haverust" == "no" ]
 then
   if [ "$abinum" -ge "060100" ]
   then
+    echo -e ">>> Removing deps and disabling rust in kernel"
     sed -i -re 's/^ (rust|bindgen|clang|llvm)(.*)/#\1\2/g' debian.master/control.stub.in
     sed -i -re "s#CONFIG_HAVE_RUST.*#CONFIG_HAVE_RUST                                policy<{'amd64': 'n'}>#" debian.master/config/annotations
     sed -i -re "s#CONFIG_RUST_IS_AVAILABLE.*#CONFIG_RUST_IS_AVAILABLE                        policy<{'amd64': 'n'}>#" debian.master/config/annotations
@@ -221,17 +235,21 @@ fi
 # force python3 to python3.9 in focal
 if [ "$series" == "focal" ]
 then
+    echo -e ">>> Forcing python3.9 on focal"
     sed -i -re 's#PYTHON=python3#PYTHON=python3.9\nexport PATH=$(shell pwd)/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin#' debian/rules
     sed -i -re 's#dh_clean#dh_clean\n\n\trm -rf bin\n\tmkdir bin\n\tln -sf /usr/bin/python3.9 bin/python3\n\tenv\n\tpython3 --version\n#' debian/rules
     sed -i -re 's# python3-dev <!stage1>,# python3-dev <!stage1>,\n python3.9-dev <!stage1>,\n python3.9-minimal <!stage1>,#g' debian.master/control.stub.in
     sed -i -re 's#PYTHON3\s*=\s*python3#PYTHON3 = python3.9#' Makefile
 fi
 
+echo -e ">>> Args.... flavour is $flavour"
 if [ "$flavour" != "none" ]
 then
+  echo -e "********\n\nSetting flavour: $flavour\n\n********"
   sed -i -re "s/(flavours\s+=).*/\1 $flavour/" debian.master/rules.d/amd64.mk
 fi
 
+echo -e ">>> Args.... exclude is $exclude"
 if [ "$exclude" != "none" ]
 then
   IFS=',' read -ra pkgs <<< "$exclude"
@@ -252,20 +270,22 @@ then
   done
 fi
 
+echo -e ">>> Args.... debug is $debug"
 if [ "$debug" == yes ]
 then
   echo -e "********\n\nEnabling Debug packages\n\n********\n"
   sed -i -re "s/skipdbg\s*=\s*true/skipdbg = false/g" debian/rules.d/0-common-vars.mk
 fi
 
+echo -e ">>> Args.... checkbugs is $checkbugs"
 if [ "$checkbugs" == "yes" ]
 then
   echo -e "********\n\nChecking for potential bugs\n\n********\n"
   if [ "$(cat debian/debian.env)" == "DEBIAN=debian.master" ]
   then
-    echo " ---> debian.env bug == no"
+    echo ">>>  ---> debian.env bug == no"
   else
-    echo " ---> debian.env bug == yes"
+    echo ">>>  ---> debian.env bug == yes"
     echo "DEBIAN=debian.master" > debian/debian.env
   fi
 fi
@@ -273,6 +293,7 @@ fi
 # Make lowlatency changes manually, the flavour was removed in 5.16.12 and newer
 if [ "$flavour" = "lowlatency" -a ! -f debian.master/control.d/vars.lowlatency ]
 then
+  echo ">>> Recreating settings for defunct lowlatency flavour"
   cat debian.master/control.d/vars.generic | sed -e 's/Generic/Lowlatency/g' > debian.master/control.d/vars.lowlatency
   touch debian.master/config/amd64/config.flavour.lowlatency
   ./scripts/config --file debian.master/config/amd64/config.common.amd64 --disable COMEDI_TESTS_EXAMPLE
@@ -286,6 +307,7 @@ then
   ./scripts/config --file debian.master/config/amd64/config.common.amd64 --set-val TEST_DIV64 m
 fi
 
+echo -e ">>> Args.... metaonly is $metaonly"
 if [ "$metaonly" == "no" ]
 then
   echo -e "********\n\nApplying default configs\n\n********"
@@ -295,12 +317,14 @@ then
   fakeroot debian/rules clean
 fi
 
+echo -e ">>> Args.... shell is $shell"
 if [ "$shell" == "yes" ]
 then
   echo -e "********\n\nPre-build shell, exit or ctrl-d to continue build\n\n********"
   bash
 fi
 
+echo -e ">>> Args.... custom is $custom"
 if [ "$custom" == "yes" ]
 then
   echo -e "********\n\nYou have asked for a custom build.\nYou will be given the chance to makemenuconfig next.\n\n********"
@@ -316,16 +340,20 @@ then
 fi
 
 echo -e "********\n\nBuilding meta package\n\n********"
+echo -e ">>> Args.... buildmeta is $buildmeta"
 if [ "$buildmeta" == "yes" ]
 then
   if [ "$flavour" == "none" ]
   then
+    echo ">>> Building generic metapackage"
     do_metapackage "${kver:1}" "${metaver}" "${metatime}" "generic" "$series" "$maintainer" "$abinum" "$btype"
     if [ -f debian.master/control.d/vars.lowlatency ]
     then
+      echo ">>> Building lowlatency metapackage"
       do_metapackage "${kver:1}" "${metaver}" "${metatime}" "lowlatency" "$series" "$maintainer" "$abinum" "$btype"
     fi
   else
+    echo ">>> Building $flavour metapackage"
     do_metapackage "${kver:1}" "${metaver}" "${metatime}" "$flavour" "$series" "$maintainer" "$abinum" "$btype"
   fi
 fi
